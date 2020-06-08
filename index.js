7020cf144c729b37e184c1d617fd32ee98a2772e
Guy Roger Eboulet <eboulet87@gmail.com>
2020-06-08 12:12:57 +0200

    Custom promise

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-alert("Coucou")
+const title = document.querySelector(".title");
+const countries = document.getElementById("countries");
+const BASE_URL = "https://covid19.mathdro.id/api";
+
+let error = null; 
+const errorDiv = document.querySelector(".error");
+let info = "";
+const infoDiv = document.querySelector(".info");
+
+function getCountries() {
+    return new Promise ((resolve, reject) => {
+        fetch( `${BASE_URL}/countries`)
+        .then(data => data.json())
+        .then(countries => {
+            console.log("coiuntries", countries);
+            resolve(countries);
+        })
+        .catch (err => {
+            reject(err);
+            errorDiv.innerText = "Impossible de récuperer la liste de pays";
+        })
+    });
+}
+
+getCountries().then(data => {
+    console.log("works");
+    
+})
+
