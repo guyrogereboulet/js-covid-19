eebce0e4d0d5422d947ce8c7fab3e82cc0453196
Guy Roger Eboulet <eboulet87@gmail.com>
2020-06-08 14:53:12 +0200

    Stats on DOM

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ let error = null;
 const errorDiv = document.querySelector(".error");
 let info = "";
 const infoDiv = document.querySelector(".info");
+const countryStatsDiv = document.querySelector(".country-stats");
+
+
+countriesSelect.addEventListener("change", e => {
+    const countryCode = e.target.value;
+    console.log(countryCode);
+    
+    getStatistics(countryCode).then(stats => {
+        errorDiv.innerText = "";
+        const countryStats = `Cas confirmés : ${stats.confirmed.value} - Guéris : ${stats.recovered.value} - Décédés : ${stats.deaths.value}`;
+        console.log("countryStats", countryStats);
+        
+        countryStatsDiv.innerText = countryStats;
+    })
+
+    .catch(err => {
+        countryStatsDiv.innerHTML = "";
+        errorDiv.innerText = err.message;
+    });
+    
+
+});
 
 
 
@@ -35,3 +57,25 @@ getCountries().then(data => {
     });
 });
 
+
+
+
+function getStatistics(countryCode) {
+    return new Promise ((resolve, reject) => {
+        fetch(`${BASE_URL}/countries/${countryCode}`)
+         .then(data => data.json())
+         .then(stats => {
+
+            console.log("stats", stats);
+            if (stats.error) {
+                throw Error(stats.error.message);
+            }
+            resolve(stats);
+            
+         })
+         .catch (err => {
+             reject(err);
+             errorDiv.innerText = `impossible de récupérer les statistiques pour ${countryCode}`;
+         })
+    });
+}
