111930d92c158710e4d22f8b35531c06b6e1d8c4
Guy Roger Eboulet <eboulet87@gmail.com>
2020-06-09 11:50:55 +0200

    Bootstrap

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ countriesSelect.addEventListener("change", e => {
     console.log(countryCode);
     
     getStatistics(countryCode).then(stats => {
-        errorDiv.innerText = "";
-        const countryStats = `Cas confirmés : ${stats.confirmed.value} - Guéris : ${stats.recovered.value} - Décédés : ${stats.deaths.value}`;
-        console.log("countryStats", countryStats);
-        
-        countryStatsDiv.innerText = countryStats;
+        // errorDiv.innerText = "";
+        // const countryStats = `Cas confirmés : ${stats.confirmed.value} - Guéris : ${stats.recovered.value} - Décédés : ${stats.deaths.value}`;
+        // console.log("countryStats", countryStats);
+        // countryStatsDiv.innerText = countryStats;
+
+        displayStatistics(stats);
     })
 
     .catch(err => {
@@ -78,4 +79,63 @@ function getStatistics(countryCode) {
              errorDiv.innerText = `impossible de récupérer les statistiques pour ${countryCode}`;
          })
     });
+}
+
+function makeTodigits(value) {
+    return value > 9 ? value : "0" + value.toString();
+}
+
+function displayStatistics(stats) {
+    const lastUpdate = new Date(stats.lastUpdate);
+    const niceDate = getLastDataUpdateDate(lastUpdate);
+    errorDiv.innerText = "";
+    const countryStats = `
+
+    <div class = "row">
+
+        <div class= "col-sm-4">
+            <div class="card">
+                <div class = "card-body">
+                <h5 class= "card-title"> Confirmés : ${stats.confirmed.value}</h5>
+                </div>
+            </div>
+        </div>
+
+
+        <div class= "col-sm-4">
+            <div class="card">
+                <div class = "card-body">
+                <h5 class= "card-title"> Guéris : ${stats.recovered.value}</h5>
+                </div>
+            </div>
+        </div>
+
+
+        <div class= "col-sm-4">
+            <div class="card">
+                <div class = "card-body">
+                <h5 class= "card-title"> Décédés : ${stats.deaths.value}</h5>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+        Données on été mis à jour le ${niceDate}
+        </div>
+    </div>
+    
+
+
+    `;
+
+    countryStatsDiv.innerHTML = countryStats;
+}
+
+function getLastDataUpdateDate(lastUpdate) {
+
+    return `${makeTodigits(lastUpdate.getDate())}/${makeTodigits(lastUpdate.getMonth()+1)}/${makeTodigits(lastUpdate.getFullYear())} à ${lastUpdate.getHours()}H${makeTodigits(lastUpdate.getMinutes())}min`;
 }
